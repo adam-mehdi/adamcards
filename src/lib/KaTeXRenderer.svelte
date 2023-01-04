5c124f67ce23e095f291624f605bb7536fa11eb1
Aidan M. Campbell <aidanmcholdcampbell@gmail.com>
2023-01-04 12:11:11 -0500

    got latex working lfg!, if this isn't good, idk what is

--- src/lib/KaTeXRenderer.svelte
+++ src/lib/KaTeXRenderer.svelte
@@ -1,3 +1,24 @@
 <script lang="ts">
-	export let cardString: string = String.raw`Let $x \in X$ be given such that $f(X) \in Y$`;
+	export let input: string = String.raw`Let $x \in X$ be given such that $f(s) \in Y$`;
+	import katex from 'katex';
+
+	const output = input
+		.replace(/\$\$(.+?)\$\$/g, (match: string, math: string) => {
+			return katex.renderToString(math, { displayMode: true, throwOnError: false });
+			// return `<KaTeX math={"${p1}"} displayMath />`;
+		})
+		.replace(/\$(.+?)\$/g, (match: string, math: string) => {
+			return katex.renderToString(math, { displayMode: false, throwOnError: false });
+		});
 </script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+		integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
+
+{@html output}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import TestPostButton from '$lib/TestPostButton.svelte';
 	import CreateDeckFromCsvButton from '$lib/CreateDeckFromCSVButton.svelte';
 	import EditableCard from '$lib/EditableCard.svelte';
+	import KaTeXRenderer from '$lib/KaTeXRenderer.svelte';
 </script>
 
 <h1>Welcome to MI/O2</h1>
@@ -25,5 +26,9 @@
 <CreateDeckFromCsvButton />
 <br />
 <hr />
+<br />
+
+<br />
+<KaTeXRenderer input={String.raw`Hello: $$x \in X$$`} />
 <br />
 <EditableCard />
