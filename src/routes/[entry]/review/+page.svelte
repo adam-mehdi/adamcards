ff5349230156f825a63f27194d405e709b9235ce
adam-mehdi <adam.mehdi@columbia.edu>
2023-01-09 20:21:41 -0700

    Review page frontend

--- src/routes/[entry]/review/+page.svelte
+++ src/routes/[entry]/review/+page.svelte
@@ -3,136 +3,116 @@
 	// import KaTeX from '$lib/KaTeX.svelte';
 	import KaTeXRenderer from '$lib/KaTeXRenderer.svelte';
 	import { invoke } from '@tauri-apps/api/tauri';
+	// import { crossfade } from 'svelte/tyes/runtime/transition';
 
+	
 	interface FrontendCard {
-		id: number;
-		front: String;
-		back: String;
+		id: number,
+		front: string,
+		back: string,
+		deck_name: string
 	}
 
-	function isFrontendCard(object: any): boolean {
-		return true;
+	interface Quotas {
+		new_left: number,
+		review_left: number,
+		num_progressed: number,
 	}
 
-	interface GetCardError {
-		type: 'NoCardsToReview' | 'OtherError';
-		message: string;
+	interface SessionState {
+		card: FrontendCard | null,
+		quotas: Quotas | null,
+		is_revealed: boolean,    // back field is revealed
+		is_finished: boolean,   // review session is completed
 	}
 
-	type GetCardResponse =
-		| { type: 'FrontendCard'; card: FrontendCard }
-		| { type: 'NoCardsToReview' }
-		| { type: 'GetCardError'; message: string };
+	let state: SessionState = {
+		card: null,
+		quotas: null,
+		is_revealed: false, 
+		is_finished: false
+	}
 
-	// ask the backend to give me the next card to review from a given deck
-	const getCard = async (deckId: number): Promise<GetCardResponse> => {
-		try {
-			// Try to get the next card
-			let c: FrontendCard = await invoke('get_next_card', { deckId });
-			return { type: 'FrontendCard', card: c };
-		} catch (err) {
-			if (typeof err === 'string') {
-				if (err === 'NoCardsToReview') {
-					return { type: 'NoCardsToReview' };
-				} else {
-					return { type: 'GetCardError', message: err };
-				}
-			} else {
-				return {
-					type: 'GetCardError',
-					message: 'unexpected non-string value returned from get_next_card'
-				};
-			}
-		}
-	};
+	async function initBoxes() {
+		let entry = $page.params.entry;
+		await invoke('init_boxes', { entry });
 
-	const postReview = (reviewScore: number) => {
-		invoke('post_review', { deckId: deckId, reviewScore: reviewScore });
-	};
+	}
+	initBoxes();
+	
+	type DrawnItems = { card: FrontendCard, quotas: Quotas};
+	async function drawCard() {
+		// draw card from a backend deck
+		const items: DrawnItems = await invoke('draw_card');
+		state.card = items.card;
+		state.quotas = items.quotas;
 
-	const handleReviewResponse = (reviewScore: number) => {
-		// synchronously
-		// post the review
-		// get the next card
-		postReview(reviewScore);
-		back = false;
-		handleGetCard();
-	};
+		// card.id == 0 ==> no more cards to review
+		if (state.card && state.card.id == 0)
+			cleanup();
 
-	// SETUP
-	let deckId = Number($page.params.deck);
+		state.is_revealed = false;
+	}
+	drawCard();
 
-	let back: boolean = false;
-	let done: boolean = false;
-
-	let card: FrontendCard | null;
-	card = null;
-	const handleGetCard = () => {
-		getCard(deckId).then((result) => {
-			if (result.type === 'FrontendCard') {
-				card = result.card;
-			} else if (result.type === 'NoCardsToReview') {
-				console.log('No Cards to review');
-				done = true;
-			} else if (result.type === 'GetCardError') {
-				console.error(result.message);
-			} else {
-				// error
-			}
+	async function handleResponse(response: number) {
+		await invoke("handle_response", {
+			"card": state.card, 
+			"quotas": state.quotas, 
+			"response": response
 		});
-	};
+	}
+	
+	// called when no more cards or user exits
+	async function cleanup() {
+		await invoke('cleanup');
+		state.is_finished = true;
+	}
 
-	handleGetCard();
-
-	// We should have some check for if there are no cards left to review that is faster
-	// than initializing a whole review session...
-
-	// ask rust for the next card and tell them the deck we're studying, rust will make sure that
-	// is the deck for which it is running a session?
+	function revealBack() {
+		state.is_revealed = true;
+	}
 
 	const onKeyDown = (e: KeyboardEvent) => {
-		console.log(`Key Code: ${e.code}`);
-		if (!back && (e.code == 'Space' || e.code == 'Enter')) {
-			back = true;
-		} else if (back) {
-			if (e.code == 'Digit1') {
-				console.log('Again');
-				handleReviewResponse(1);
-			} else if (e.code == 'Digit2') {
-				console.log('Good');
-				handleReviewResponse(2);
-			} else if (e.code == 'Digit3') {
-				console.log('Easy');
-				handleReviewResponse(3);
-			}
+		// console.log(`Key Code: ${e.code}`);
+		if (!state.is_revealed) {
+			state.is_revealed = true;
+		} else {
+			if (e.code == 'Digit1') 
+				handleResponse(1);
+			else if (e.code == 'Digit2')
+				handleResponse(2);
+			else if (e.code == 'Digit3')
+				handleResponse(3);
 		}
 	};
+	console.log(state);
+
+
 </script>
 
 <!-- Listen for keyboard events -->
 <svelte:window on:keydown|preventDefault={onKeyDown} />
 <a href="/">Home</a>
-<p>Studying Deck: {deckId}</p>
 
-{#if !done}
-	<!-- <h3>{card ? card.front : ''}</h3> -->
+{#if !state.is_finished}
 
 	<div>
-		<KaTeXRenderer input={card ? card.front.toString() : ''} />
+		<KaTeXRenderer input={state.card ? state.card.front.toString() : ''} />
 	</div>
-	<div class={back ? '' : 'hidden'}>
-		<KaTeXRenderer input={card ? card.back.toString() : ''} />
+	<div class={state.is_revealed ? '' : 'hidden'}>
+		<KaTeXRenderer input={state.card ? state.card.back.toString() : ''} />
 	</div>
 
 	<!-- <h3 class={back ? '' : 'hidden'}>{card ? card.back : ''}</h3> -->
 	<br />
-	<div class={back ? '' : 'hidden'}>
-		<button on:click={() => handleReviewResponse(1)}>Again</button>
-		<button on:click={() => handleReviewResponse(2)}>Good</button>
-		<button on:click={() => handleReviewResponse(3)}>Easy</button>
+	<div class={state.is_revealed ? '' : 'hidden'}>
+		<button on:click={() => handleResponse(1)}>Again</button>
+		<button on:click={() => handleResponse(2)}>Hard</button>
+		<button on:click={() => handleResponse(3)}>Good</button>
 	</div>
 {:else}
-	<h1>No more cards to review for deck {deckId}!</h1>
+	<h1>Congrats! You've finished reviewing {$page.params.entry}</h1>
 {/if}
 
 <style>
